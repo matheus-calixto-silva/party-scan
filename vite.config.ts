347feee689fb0c8f18d2b1a4d935dae70af7481e
Matheus Calixto <matheuscalixto8@gmail.com>
2024-06-10 16:55:09 -0300

    chore: config do path alias do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import path from 'node:path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src', 'app'),
+      '@views': path.resolve(__dirname, 'src', 'views'),
+    },
+  },
+});
