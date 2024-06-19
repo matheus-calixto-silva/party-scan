3e71f90057f8383cf43bfc0a8fcf977e39483f3e
Matheus Calixto <matheuscalixto8@gmail.com>
2024-06-19 12:26:12 -0300

    feat: inicializa o componente App com providers de tema e convidados

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { GuestProvider } from '@app/contexts/GuestContext';
+import { ThemeProvider } from '@app/contexts/ThemeProvider';
+import Home from '@views/pages/Home';
+
 const App = () => (
-  <div>
-    <h1>Party Scan</h1>
-  </div>
+  <ThemeProvider>
+    <GuestProvider>
+      <div className="container mx-auto py-10">
+        <Home />
+      </div>
+    </GuestProvider>
+  </ThemeProvider>
 );
 
 export default App;
