6cd6822c7b21967ac7557cd36c2b1cee9c729af0
Matheus Calixto <matheuscalixto8@gmail.com>
2024-06-19 12:20:57 -0300

    feat(components): adiciona o componente Home com Header e GuestTable

---
+++ src/views/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import GuestTable from '@views/components/GuestTable';
+import Header from '@views/components/Header';
+
+const Home = () => (
+  <section className="w-full h-screen">
+    <Header />
+    <GuestTable />
+  </section>
+);
+
+export default Home;
